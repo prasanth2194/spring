775acbd2e93224153fdb9b79249911e63c2bc5ae
prasanth m <prasanth.chinna21@gmail.com>
2019-11-25 23:20:17 +0530

    deployed by prasanth

---
+++ ArrayList_Demo/src/com/prasanth/alist/ArrayListDemo_1.java
@@ -0,0 +1,50 @@
+package com.prasanth.alist;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class ArrayListDemo_1 {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("sam");
+		al.add("ram");
+		al.add("pom");
+
+		Iterator it = al.iterator(); // iterator is used to print elements on
+										// after the other
+
+		while (it.hasNext()) { // hasNext() checks wheather elements present or
+								// not
+			System.out.println(it.next()); // returns next element in the
+											// arraylist
+			/* continue; */
+		}
+		System.out.println("Elements after adding");
+		al.add(2, "jam");
+
+		Iterator it1 = al.iterator();
+
+		while (it1.hasNext()) {
+			System.out.println(it1.next());
+		}
+		System.out.println("Elements after removing");
+		al.remove("pom"); // remove the element
+		al.set(2, "ramu"); // update the element
+		int pos=al.indexOf("ramu"); // return index  present or not default returns -1
+		System.out.println(pos);
+		String str=al.get(2);	//	returns the element present at particular index
+		System.out.println(str);
+		Collections.sort(al);
+		
+
+		Iterator it2 = al.iterator();
+
+		while (it2.hasNext()) {
+			System.out.println(it2.next());
+		}
+
+	}
+}
